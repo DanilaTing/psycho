752dab769c10cdcc8f4b5f152be0223cc802c8d1
DanilaTing <tinggone@gmail.com>
2019-03-26 03:46:01 +0300

    not copile

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
+config.assets.initialize_on_precompile = false
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -9,7 +9,6 @@ Bundler.require(*Rails.groups)
 module Psycho
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.assets.initialize_on_precompile = false
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
